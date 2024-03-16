b9210b01ddb3830dd3a0ac192661db68c9e32521
Xury Greer <xury.greer@gmail.com>
2024-03-15 20:03:48 -0700

    Convert iqm_export_pipeline.py into a Blender add-on with bl_info, register/unregister functions, and an IQMExportPipeline operator

--- iqm_export_pipeline.py
+++ iqm_export_pipeline.py
@@ -1,24 +1,55 @@
+bl_info = {
+    "name" : "IQM Export Pipeline",
+    "author" : "Xury Greer",
+    "version" : (1, 0),
+    "blender" : (3, 6, 0),
+    "location" : "Armature > Data > IQM",
+    "description" : "A pipeline that offers quality-of-life improvements for using the IQM exporter",
+    "category" : "Import-Export",
+}
+
 import os
 import bpy
 from iqm_export import exportIQM
 
-file_directory = f"C:\\Users\\{os.environ.get('USERNAME')}\\Documents\\"
-file_name = "iqm_export_test"
-file_extention = ".iqm"
+class IQMExportPipeline(bpy.types.Operator):
+    """Run the exportIQM function with pre-defined pipeline options"""
+    bl_idname = "export.iqm_pipeline"
+    bl_label = "Export IQM via pipeline"
 
-animations_to_export = "idle::::1, walk::::1, run::::1"
+    def execute(self, context):
+        file_directory = f"C:\\Users\\{os.environ.get('USERNAME')}\\Documents\\"
+        file_name = "iqm_export_test"
+        file_extention = ".iqm"
 
-exportIQM(
-    context = bpy.context,
-    filename = os.path.join(file_directory, file_name + file_extention),
-    usemesh = True,
-    usemods = True,
-    useskel = True,
-    usebbox = True,
-    usecol = False,
-    scale = 1.0,
-    animspecs = animations_to_export,
-    matfun = (lambda prefix, image: prefix),
-    derigify = False,
-    boneorder = None
-    )
+        animations_to_export = "idle::::1, walk::::1, run::::1"
+
+        exportIQM(
+            context = bpy.context,
+            filename = os.path.join(file_directory, file_name + file_extention),
+            usemesh = True,
+            usemods = True,
+            useskel = True,
+            usebbox = True,
+            usecol = False,
+            scale = 1.0,
+            animspecs = animations_to_export,
+            matfun = (lambda prefix, image: prefix),
+            derigify = False,
+            boneorder = None
+            )
+
+        return {'FINISHED'}
+
+classes = [IQMExportPipeline]
+
+def register():
+    for class_to_register in classes:
+        bpy.utils.register_class(class_to_register)
+
+def unregister():
+    for class_to_unregister in classes:
+        bpy.utils.unregister_class(class_to_unregister)
+
+if __name__ == "__main__":
+    register()
