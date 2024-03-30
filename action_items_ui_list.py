07ac4904e958edf4ef2bf18fe8c274b9dec9c8c1
Xury Greer <xury.greer@gmail.com>
2024-03-30 16:22:21 -0700

    Update the ACTIONITEMS_UL_ActionItemList draw_item function to include frame_start, frame_end, and fps

--- action_items_ui_list.py
+++ action_items_ui_list.py
@@ -44,8 +44,11 @@ class ACTIONITEMS_UL_ActionItemList(UIList):
         if self.layout_type in {"DEFAULT", "COMPACT"}:
             row = layout.row()
             row.label(icon="ARMATURE_DATA")
-            row.prop(data=item, property="looping", text="Looping")
             row.prop(data=item, property="action", text="")
+            row.prop(data=item, property="frame_start", text="Start")
+            row.prop(data=item, property="frame_end", text="End")
+            row.prop(data=item, property="fps", text="FPS")
+            row.prop(data=item, property="looping", text="Looping")
 
         elif self.layout_type in {"GRID"}:
             layout.alignment = "CENTER"
