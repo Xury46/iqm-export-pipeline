2e7130eb2cfa2d73e8bb3ecf0b146b9c663569cc
Xury Greer <xury.greer@gmail.com>
2024-03-31 14:18:17 -0700

    Add bundled transform offset presets

---
+++ presets/No_Offset.py
@@ -0,0 +1,6 @@
+import bpy
+scene = bpy.context.scene
+
+scene.iqm_export_pipeline_settings.offset_location = (0.0, 0.0, 0.0)
+scene.iqm_export_pipeline_settings.offset_rotation = (0.0, 0.0, 0.0)
+scene.iqm_export_pipeline_settings.offset_scale = (1.0, 1.0, 1.0)
---
+++ presets/Quake.py
@@ -0,0 +1,6 @@
+import bpy
+scene = bpy.context.scene
+
+scene.iqm_export_pipeline_settings.offset_location = (0.0, 0.0, -24.0)
+scene.iqm_export_pipeline_settings.offset_rotation = (0.0, 0.0, 1.5707963705062866)
+scene.iqm_export_pipeline_settings.offset_scale = (32.0, 32.0, 32.0)
