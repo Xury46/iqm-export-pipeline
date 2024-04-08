843d6ef19d65891922215712c9081c787aa372ad
Xury Greer <xury.greer@gmail.com>
2024-04-07 18:36:07 -0700

    Add function for installing presets

--- pipeline_presets.py
+++ pipeline_presets.py
@@ -1,7 +1,35 @@
+import os
+import shutil
+import filecmp
 import bpy
 from bpy.types import Menu, Operator, Panel
 from bl_ui.utils import PresetPanel
 from bl_operators.presets import AddPresetBase
+
+
+def install_presets():
+    # https://sinestesia.co/blog/tutorials/using-blenders-presets-in-python/
+    bundled_presets_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "presets")
+    presets_install_dir = os.path.join(bpy.utils.user_resource("SCRIPTS"), "presets", "iqm_export_pipeline")
+
+    if not os.path.isdir(presets_install_dir):
+        os.makedirs(presets_install_dir)
+
+    bundled_presets = os.listdir(bundled_presets_dir)
+
+    for preset_file in bundled_presets:
+
+        bundled_file = os.path.join(bundled_presets_dir, preset_file)
+        installed_file = os.path.join(presets_install_dir, preset_file)
+
+        # Check if the file is already installed.
+        if os.path.isfile(installed_file):
+            # Check if the installed file is up-to-date.
+            if filecmp.cmp(bundled_file, installed_file):
+                continue
+
+        # Copy the file, overwrite the out-of-date file if it exists.
+        shutil.copy2(os.path.join(bundled_presets_dir, preset_file), presets_install_dir)
 
 
 class IQM_EXPORT_PIPELINE_MT_TransformOffsetPresets(Menu):
@@ -49,6 +77,8 @@ classes = [
 def register():
     for class_to_register in classes:
         bpy.utils.register_class(class_to_register)
+
+    install_presets()
 
 
 def unregister():
