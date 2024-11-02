ecd0804ecf0dc1dc92b67872cbd839f459077779
Xury Greer <xury.greer@gmail.com>
2024-11-02 13:00:10 -0700

    Increment add-on version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "IQM Export Pipeline",
     "author": "Xury Greer",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 6, 0),
     "location": "Armature > Data > IQM",
     "description": "A pipeline that offers quality-of-life improvements for using the IQM exporter",
